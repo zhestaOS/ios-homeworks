fa57ff4612d22d3d986db07ec5e3894285b07182
Evgeniya Shevyakova <stativa.zhe@gmail.com>
2021-11-27 21:10:40 +0300

    ProfileView added

---
+++ Navigation/ProfileView.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileView.swift
+//  Navigation
+//
+//  Created by Евгения Статива on 27.11.2021.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    @IBOutlet weak var userFoto: UIImageView!
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var dateOfBirth: UILabel!
+    @IBOutlet weak var cityOfBirth: UILabel!
+    @IBOutlet weak var userDescription: UITextView!
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
